b4cb7ae44c18f78131f942c47121c6e8849516e8
Jon Borgonia <jon@byhumanhand.com>
2011-01-23 17:53:15 -1000

    ImportClassesMacro writes imports for Helper classes

--- engine/ImportClassesMacro.hx
+++ engine/ImportClassesMacro.hx
@@ -6,12 +6,21 @@ class ImportClassesMacro {
     for(controller_file_name in controller_files){
       controllers.push("import controllers."+controller_file_name.substr(0,controller_file_name.indexOf(".hx"))+";");
     }
-    var imploded = controllers.join("\n");
+    
+    var helper_files = neko.FileSystem.readDirectory("./app/helpers/");
+    var helpers = new Array<String>();
+    for(helper_file_name in helper_files){
+      helpers.push("import helpers."+helper_file_name.substr(0,helper_file_name.indexOf(".hx"))+";");
+    }
+    
+    var imploded = controllers.join("\n")+"\n";
+    imploded += helpers.join("\n");
     imploded += "\nclass ImportClasses{}";
 
     var file = neko.io.File.write("./engine/ImportClasses.hx", false);
     file.writeString(imploded);
     file.close();
+    
 
     return { expr : EConst(CString(imploded)), pos : haxe.macro.Context.currentPos() };
   }
