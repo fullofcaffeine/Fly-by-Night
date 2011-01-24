8ec1cb7df1ddd0fa8ad021ca3575759bbe380a48
Jon Borgonia <jon@byhumanhand.com>
2011-01-23 17:55:36 -1000

    AeroHelper holds default variables for being rendered in views

--- engine/AeroHelper.hx
+++ engine/AeroHelper.hx
@@ -1,4 +1,10 @@
 class AeroHelper
 {
-
+  public var controller: AeroController;
+  public var yield: String;
+  
+  public function new( controller:AeroController )
+  {
+    this.controller = controller;
+  }
 }
