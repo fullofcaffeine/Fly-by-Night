09294304cdfda93383b2aad23558451a4da68dc1
Jon Borgonia <jon@byhumanhand.com>
2011-02-09 23:09:05 -1000

    AeroController creates a params object to be used across actions. AeroController will not create views (by default) when not needed.

--- engine/AeroController.hx
+++ engine/AeroController.hx
@@ -6,12 +6,17 @@ class AeroController
   public var action: String;
   public var content: Hash<Dynamic>;
   public var helper: AeroHelper;
+  public var params: Hash<String>;
+  public var obj_params: Hash<String>;
   
-  public function new( action:String )
+  public function new( action:String, params:Hash<String> )
   {
     name = Type.getClassName(Type.getClass(this)).substr(12); // controller.
     this.action = action;
     content = new Hash<Dynamic>();
+    this.params = params;
+    var default_obj_name = Utils.singularize(name.toLowerCase());
+    obj_params = getObjParams(default_obj_name);
     
     if(FileSystem.exists(Settings.get("FBN_ROOT")+"app/helpers/"+name+".hx")){
       helper = Type.createInstance(Type.resolveClass("helpers."+name),[this]);
@@ -21,8 +26,22 @@ class AeroController
       helper = new AeroHelper(this);
     }
     
-    view = new AeroView(this);
+    if(action != "create" && action != "update" && action != "destroy")
+      view = new AeroView(this);
   }
-
   
+  private static inline function getObjParams( obj:String ): Hash<String>
+	{
+	  var h = new Hash<String>();
+	  var keys = php.Web.getParamValues(obj+"_keys");
+	  var vals = php.Web.getParamValues(obj);
+	  if(keys != null && vals != null){
+	    var l = keys.length;
+  	  for(i in 0...l){
+  	    h.set(keys[i], vals[i]);
+  	  }
+	  }
+	  return h;
+	}
+
 }
