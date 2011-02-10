ac6002568043883e5653a69f9272206170628865
Jon Borgonia <jon@byhumanhand.com>
2011-02-09 23:04:48 -1000

    AeroModel extends db.Object , doesn't really do anything yet

--- engine/AeroModel.hx
+++ engine/AeroModel.hx
@@ -1,4 +1,11 @@
-class AeroModel
+import php.FileSystem;
+import php.db.Object;
+class AeroModel extends db.Object
 {
-
+  public var name: String;
+  public function new( )
+  {
+    name = Type.getClassName(Type.getClass(this)).substr(8); // models.
+    
+  }
 }
