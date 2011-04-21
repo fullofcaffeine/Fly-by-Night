14ea8ae300ee33f4dc9fd299a617cd67178e6dd2
Jon Borgonia <jon@byhumanhand.com>
2011-04-21 05:48:15 -1000

    override toString() to AeroModel for much nicer output. also added public toHash() method; models.Post => { 	id => 1 	title => test post 	body => test body content }

--- engine/AeroModel.hx
+++ engine/AeroModel.hx
@@ -8,14 +8,12 @@ import php.db.Manager;
 import db.DBConnection;
 class AeroModel extends php.db.Object
 {
-/*  public var name: String;*/
   public var id: Int;
-  public var manager: Dynamic;
   public function new( )
   {
     DBConnection.connection;
 /*    name = Type.getClassName(Type.getClass(this)).substr(8); // models.*/
-    manager = new Manager(cast Type.resolveClass(Type.getClassName(Type.getClass(this))));
+    __manager__ = new Manager(cast Type.resolveClass(Type.getClassName(Type.getClass(this))));
     
     super();
       
@@ -80,6 +78,27 @@ class AeroModel extends php.db.Object
 /*      result = manager.objects("SELECT * FROM posts", false);*/
     }
     return result;
+  }
+  
+  public override function toString(  ):String
+  {
+    var out = Type.getClass(this) + " => {\n";
+    var hash = this.toHash();
+    for(key in hash.keys()){
+      out += "\t"+key+" => "+hash.get(key)+"\n";
+    }
+    out += "}";
+    //return this.toHash().toString();
+    return out;
+  }
+  public inline function toHash(  ):Hash<Dynamic>
+  {
+    var fields = Lambda.filter(Reflect.fields(this),function(f){ return !StringTools.startsWith(f,"__"); });
+    var fields_hash = new Hash<Dynamic>();
+    for(field in fields){
+      fields_hash.set(field,Reflect.field(this,field));
+    }
+    return fields_hash;
   }
   
 }
