5d37c61c506ff2d5c4efd1f424829ca947bc122a
Jon Borgonia <jon@byhumanhand.com>
2011-10-03 15:02:00 -1000

    added = render_partial() helper method to AeroHelper which works pretty much in the same way that rails handles rendering partials. right now only takes a single argument, and uses the AeroController.content hash for resolving any dynamic content. from 'Posts' controller, any action's view = render_partial("footer") will render the '/app/views/posts/_footer.haml' file from any controller/action, = render_partial("layouts/nav") will render the '/app/views/layouts/_nav.haml' file

--- engine/AeroHelper.hx
+++ engine/AeroHelper.hx
@@ -2,6 +2,9 @@
   To see a full list of auto imported classes for all subclasses of AeroHelper
   See RunwayMacros.PREPEND_TO_HELPER 
 */
+import php.FileSystem;
+import php.io.File;
+import haml_crate.HamlHX;
 class AeroHelper
 {
   public var controller: AeroController;
@@ -13,5 +16,63 @@ class AeroHelper
   public function new( controller:AeroController )
   {
     this.controller = controller;
+  }
+  
+  public function render_partial( partial_path:String ):String
+  {
+    var partial_filename = "";
+    var partial = "";
+    if(partial_path.indexOf("/") == -1){
+      // check if partial path is valid
+      if(StringTools.startsWith(partial_path,"_")){
+        throw "Invalid Partial template name '"+partial_path+"' must not begin with underscore '_', for "+controller.name+":"+controller.action;
+      }
+      partial_path = Utils.to_underscore(controller.name)+"/_"+partial_path;
+    }else{
+      
+      var partial_path_parts = partial_path.split("/");
+      partial_filename = partial_path_parts.pop();
+      
+      // check if partial path is valid
+      if(StringTools.startsWith(partial_filename,"_")){
+        throw "Invalid Partial template name '"+partial_filename+"' must not begin with underscore '_', for "+controller.name+":"+controller.action;
+      }
+      
+      // add the underscore _
+      partial_path = partial_path_parts.join("/")+"/_"+partial_filename;
+    }
+    
+    // extension is not included
+    if(partial_path.indexOf(".") == -1){
+      var partial_file_found = false;
+      for(ext in Template.types.keys()){
+        if(!partial_file_found){
+          
+          partial_filename = Settings.get("FBN_ROOT")+"app/views/"+partial_path+"."+ext;
+          if(FileSystem.exists(partial_filename)){
+            partial_file_found = true;
+            break;
+          }
+        }
+      }
+      
+      if(!partial_file_found){
+        throw "Partial template for "+controller.name+":"+controller.action+" not found at "+Settings.get("FBN_ROOT")+"app/views/"+partial_path+"<br />Must be a supported template type: "+Template.types.toString();
+      }
+      
+    }else{
+      partial_filename = partial_path;
+    }
+    
+    if(FileSystem.exists(partial_filename)){
+      partial = File.getContent(partial_filename);
+    }else{
+      throw "Template for "+controller.name+":"+controller.action+" not found at "+Settings.get("FBN_ROOT")+"app/views/"+Utils.to_underscore(controller.name)+"/"+Utils.to_underscore(partial_filename)+"<br />Must be a supported template type: "+Template.types.toString();
+    }
+    
+    
+    
+    
+    return HamlHX.haml2html(partial, partial_filename, controller.content, this);
   }
 }
