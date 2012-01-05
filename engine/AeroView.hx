d65af8deba352d6c0cb38ae4bf04e25591f6c88a
theRemix <jon@byhumanhand.com>
2012-01-04 16:49:00 -1000

    merged in AeroView.render_text(text:String,?layout:String) from some other branch/project

--- engine/AeroView.hx
+++ engine/AeroView.hx
@@ -27,6 +27,41 @@ class AeroView
     this.controller = controller;
   }
   
+  public function render_text( text:String, ?custom_layout:String ):Void
+  {
+    var type_ext = ".haml"; // TODO cycle through default types for matching file
+    
+    if(custom_layout != null){
+      var layout_filename = Settings.get("FBN_ROOT")+"app/views/layouts/"+Utils.to_underscore(custom_layout)+type_ext;
+      if(FileSystem.exists(layout_filename)){
+        layout = File.getContent(layout_filename);
+      }else{
+        throw "Layout "+custom_layout+" doesn't exist at: "+layout_filename;
+      }
+      
+      content.set("yield",text);
+      #if nodejs
+        controller.res.write(
+      #else
+        Lib.print(
+      #end
+        HamlHX.haml2html(layout, layout_filename, content, helper)
+      );
+      
+      
+      
+    }else{
+      #if nodejs
+        controller.res.write(
+      #else
+        Lib.print(
+      #end
+        text
+      );
+    }
+    rendered = true;
+  }
+  
   public function render( ?custom_layout:String, ?custom_template:String, ?custom_content:Hash<Dynamic>, ?custom_helper:AeroHelper ):Void
   {
     var type_ext = ".haml"; // TODO cycle through default types for matching file
@@ -198,10 +233,13 @@ class AeroView
     var r:TemplateType;
     if(StringTools.endsWith(file_name, ".haml")){
       r = TemplateType.HAML;
-    }else if(StringTools.endsWith(file_name, ".mtt")){
-      r = TemplateType.TEMPLO;
-    }else if(StringTools.endsWith(file_name, ".xml")){
-      r = TemplateType.XML;
+      
+//    Not yet implemented
+//    }else if(StringTools.endsWith(file_name, ".mtt")){
+//      r = TemplateType.TEMPLO;
+//    }else if(StringTools.endsWith(file_name, ".xml")){
+//      r = TemplateType.XML;
+
     }else{
       throw "Template Error! '"+file_name+"' is not one of "+Type.getEnumConstructs(TemplateType).toString();
     }
