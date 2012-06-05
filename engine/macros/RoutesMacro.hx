1d5e9c6db35cb63de09dfc44af8e60a50fb5ba63
Jon Borgonia <jon@gomagames.com>
2012-06-05 13:49:30 -1000

    fixed RoutesMacro to allow routes.yml to have map routes that override rest autocreated routes

--- engine/macros/RoutesMacro.hx
+++ engine/macros/RoutesMacro.hx
@@ -2,27 +2,29 @@ package macros;
 import yaml_crate.YamlHX;
 import haxe.macro.Expr;
 class RoutesMacro {
+  private static var names: Array<String>;
+  
   @:macro public static function write() : Expr {
     var imploded = "";
     if(neko.FileSystem.exists('./config/routes.yml')){
       // check if config has db setup for env
       var routes_yml = YamlHX.read(neko.io.File.getContent('./config/routes.yml'));
-      var names = new Array<String>();
+      names = new Array<String>();
       var name:String;
       // find route
       for (e in routes_yml.elements ){
         if(e.name == "map"){
           if(e.hasNode.name)
-            names.push(e.node.name.innerData);
+            names_push(e.node.name.innerData);
         }else if(e.name == "rest"){
           name = Std.string(Utils.singularize(Utils.to_underscore(e.innerData))).toLowerCase();
-          names.push(Utils.pluralize(name));
-          names.push(name);
-          names.push("new_"+name);
-          names.push("create_"+name);
-          names.push("edit_"+name);
-          names.push("update_"+name);
-          names.push("destroy_"+name);
+          names_push(Utils.pluralize(name));
+          names_push(name);
+          names_push("new_"+name);
+          names_push("create_"+name);
+          names_push("edit_"+name);
+          names_push("update_"+name);
+          names_push("destroy_"+name);
         }
       }
       
@@ -34,6 +36,13 @@ class RoutesMacro {
       file.close();
     }
     return { expr : EConst(CString(imploded)), pos : haxe.macro.Context.currentPos() };
+  }
+  
+  private static inline function names_push( name:String ):Void
+  {
+    if(!Lambda.has(names, name)){
+      names.push(name);
+    }
   }
  
 }
